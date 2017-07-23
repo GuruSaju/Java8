44820e453c868646bddf19a7443c0eb26c172a71
Srisarguru Sridhar <srisargurusridhar@u.boisestate.edu>
2017-07-23 19:10:18 -0400

    grreting

--- src/guru/java8lambda/com/Greeter.java
+++ src/guru/java8lambda/com/Greeter.java
@@ -7,21 +7,44 @@ public class Greeter {
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Greeter greeter = new Greeter();
 		HelloWorldGreeting helloWorldGreeting = new HelloWorldGreeting();
 		greeter.greet(helloWorldGreeting);
 		
-		//looks for a method with the same signature in the interface 
-		MyLambda myLambdaFunction = () -> System.out.println("Hello World!");
+		//looks for a method with the same signature in the interface
+		//compiler does the type checking
+		MyLambda1 myLambdaFunction = () -> System.out.println("Hello World!");
 		
+		//MyLambda addFunction = (int a, int b) -> a + b; This would give an error because for lambda 
+		//functions you cannot have more than one function in an interface so another interface;
+		
+		
+		//This works
+		MyLambda2 addFunction = (int a, int b) -> a + b;
+		
+		
+		//Since we already have an interface greeter
+		Greeting greetingLambdaFunction = () -> System.out.println("Hello from greeter");
+	
+		Greeting helloWorldGreeting1 = new HelloWorldGreeting();
+		
+		//Difference between above two lines??
 	}
 
 }
 
 //Step 1
 
-interface MyLambda{
+interface MyLambda1{
 	//Step 2 (exact same method signature as the lambda function you want to create)
 	void anyName();
+	
+	//for add function
+	//int anyName1(int x,int y);
+}
+
+interface MyLambda2{
+	int anyName(int x,int y);
+
 }
