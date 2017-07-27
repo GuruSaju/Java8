4b5e98c80723ca3207dddfcbdb60ced0269f7d86
Srisarguru Sridhar <srisargurusridhar@u.boisestate.edu>
2017-07-27 09:57:46 -0400

    "more info for understanding"

--- src/guru/lambdaclosures/com/ClosureJava8.java
+++ src/guru/lambdaclosures/com/ClosureJava8.java
@@ -8,7 +8,8 @@ public class ClosureJava8 {
 	
 	public static void main(String[] args) {
 		int a =10;
-		int b=20;  
+		int b=20; 
+		//no need to declare b a final because b is automatically froze by lambda.
 		doProcess(a, i -> System.out.println(i+b)); //freezes the value of b that is passed so b does not changes. this is closure.
 
 	}
